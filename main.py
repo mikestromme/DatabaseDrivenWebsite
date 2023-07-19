53cab5e25b12aeb50d96cc10b13adaa13ce81737
Mike Stromme <mstromme@suburbanenterprises.com>
2023-07-19 16:54:21 -0500

    x

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
 load_dotenv()
 database_server = os.getenv("database_server")
@@ -24,17 +25,19 @@ db = SQLAlchemy(app)
 def home():
     return render_template('home.html')
 
+
 @app.route('/form1', methods=['GET', 'POST'])
 def form1():
     if request.method == 'POST':
         param1 = request.form.get('param1')
         param2 = request.form.get('param2')
+        param3 = ''
 
-        proc_name = 'my_stored_proc_1'
-        params = {'param1': param1, 'param2': param2}
+        proc_name = text('EXEC tc_create_flask :param1, :param2, :param3') # Use text() to wrap the stored procedure call
+        params = {'param1': param1, 'param2': param2, 'param3': param3}
 
         try:
-            result = db.session.execute(proc_name, params)
+            result = db.session.execute(proc_name, params) # This should now correctly call your stored procedure
             db.session.commit()
 
             # If no errors, redirect to a success page
@@ -45,6 +48,7 @@ def form1():
 
     # Render the form
     return render_template('form1.html')
+
 
 @app.route('/form2', methods=['GET', 'POST'])
 def form2():
@@ -73,6 +77,29 @@ def form2():
 @app.route('/success')
 def success():
     return 'Form successfully submitted!'
+
+
+@app.route('/execute_proc', methods=['POST'])
+def execute_proc():
+    data = request.get_json()
+
+    # Stored procedure name.
+    proc_name = data['proc_name']
+
+    # The parameters are expected to be passed in the request as a dictionary.
+    # Each key is a parameter name and each value is the corresponding parameter value.
+    params = data['params']
+
+    try:
+        result = db.session.execute(proc_name, params)
+        db.session.commit()
+
+        # If the procedure is supposed to return something, you can fetch it with result.fetchall()
+
+        return {'status': 'success'}, 200
+    except Exception as e:
+        return {'status': 'error', 'message': str(e)}, 400
+
 
 if __name__ == '__main__':
     app.run(debug=True)
